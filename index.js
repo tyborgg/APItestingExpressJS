80120564301cf01fac3061eaca2905915400392d
Tyler Pastor <72320023+tyborgg@users.noreply.github.com>
2021-07-23 15:21:14 -0700

    Integrated UMLSJS

--- index.js
+++ index.js
@@ -1,59 +1,130 @@
-/*const express = require("express");     //import express js module into the program
-const app = express();                  //creates instance of express in the the value app
-                                        //req = input   res = output
-app.get("/hi", (req, res)=> {           //configuring the route of the action "Get" by outputting 'hello world'
-
-    function hello(){
-        return "hello world";
-    }
-
-    res.send(hello());                  //to the path '/hi'
-});
-                                        //Actions: GET  POST  PUT  PATCH  DELETE 
-//const port = process.env.port || 3000;
-app.listen(3000);                       //starts the application on port 3000 
-                                        //use a router to keep code clean, easy to maintain, easy to fix bugs, etc.
-                                        //using express generator a template creates app.js which is 
-                                        //the "main" file of the entire application
-                                        //a "npm i" must be run to include the dependencies for the application
-
-*/
 const express = require("express");
-const definitions = require('./routes/definitions');
+const {UMLSJS} = require("umlsjs");
 const app = express();
-const puppeteer = require('puppeteer');
+const definitions = require('./routes/definitions');
+const request = require("request");
+const fs = require('fs');
+const cors = require("cors");
+const run = require("./search.js");
+const { callbackify } = require("util");
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
 
-var rawTxt;
+var input = [];
+var CUI = [];
 
-async function TicketGrabber(){
-    const browser = await puppeteer.launch({
-        headless: true
-    });
-    const page = await browser.newPage();
-    await page.goto('https://documentation.uts.nlm.nih.gov/rest/authentication.html');
-    await page.type('#apikey', 'e588486b-ebc2-4edd-b6d0-b394d6fec87b');
-    await page.click('#getTGTBtn');
-    await page.waitForTimeout(750);
-    await page.click('#getSTBtn');
-    await page.waitForTimeout(750);
+async function STGrabber(){
+    const api_key = process.env.APIKEY;
+    const token = new UMLSJS.UMLSToken(api_key);
+    let ST = await token.getSt();
 
-    const [el] = await page.$x('//*[@id="stVal"]');
-    const txt = await el.getProperty('textContent');
-    rawTxt = await txt.jsonValue();
-
-    console.log(rawTxt);
-
-    //InfoGrabber(rawTxt);
-
-    browser.close();
+    return ST;
 }
 
-TicketGrabber();
+async function CUIGrabber(val){
+    var ST = await STGrabber();
+    const search = new UMLSJS.UMLSSearch(ST);
+    search.init(val);
+    await search.query();
+    const result = search.getResults();
 
-app.get("/", (req, res)=> {           
-    res.send(rawTxt);                  
+    return result;
+}
+
+async function DefGrabber(){
+    var ST = await STGrabber();
+    const search = new UMLSJS.CUISearch(ST);
+    search.init(CUI[0]);
+    await search.getDefinitions()
+    const result = search.definitions;
+
+    return result;
+}
+
+async function AUIGrabber(){
+    var ST = await STGrabber();
+    const search = new UMLSJS.CUISearch(ST);
+    search.init(CUI[0]);
+    await search.getAtoms()
+    const result = search.atoms;
+
+    return result;
+}
+
+app.post('/search', async(req,res)=>{
+    var searchVal = req.body.searchVal;
+    input = [];
+    input.push(searchVal);
+    console.log(searchVal);
+    // var searchVal = req.body.searchVal;
+    // fs.writeFile('userSearch.txt', searchVal, (err) => {
+    //     if (err) throw err;
+    // });
+    // fs.writeFile('CUI.txt', "", (err) => {
+    //     if (err) throw err;
+    // });
+    // fs.writeFile('AUI.txt', "", (err) => {
+    //     if (err) throw err;
+    // });
+    // fs.writeFile('definition.txt', "", (err) => {
+    //     if (err) throw err;
+    // });
+    // run.TGTGrabber();
+    //res.send(searchVal);
+}); 
+
+app.get("/", (req, res)=> {          
+    res.send(input);              
 });
 
-app.use('/definitions', definitions);
+app.get("/api/CUIs", (req, res)=> {
+    // setTimeout(async function () { //text file delay call
+    //     val = fs.readFileSync('CUI.txt', 'utf8');
+    //     res.json({CUI: val});
+    // }, 4000 );
 
-app.listen(3000);
+    setTimeout(async function () { //text file delay call
+        var tmpCUI = await CUIGrabber(input[0]);
+        var val = tmpCUI[0].ui;
+        CUI = [];
+        CUI.push(val);
+        console.log("CUI", val);
+        res.json({CUI: val});
+    }, 200 );
+});
+
+app.get("/api/definitions", (req, res)=> {
+    // setTimeout(async function () { //text file delay call
+    //     val = fs.readFileSync('definition.txt', 'utf8');
+    //     res.json({def: val});
+    // }, 4000 );
+    setTimeout(async function () { //text file delay call
+        var tmpDef = await DefGrabber();
+        var val = tmpDef[0].value;
+        console.log("Def", val);
+        res.json({def: val});
+    }, 1000 );
+});
+
+app.get("/api/AUIs", (req, res)=> {
+    // setTimeout(async function () { //text file delay call
+    //     val = fs.readFileSync('AUI.txt', 'utf8');
+    //     res.json({AUI: val});
+    // }, 4000 );
+    setTimeout(async function () { //text file delay call
+        var tmpAUI = await AUIGrabber();
+        var val = tmpAUI[0].ui;
+        console.log("AUI", val);
+        res.json({AUI: val});
+    }, 1000 );
+});
+
+// app.get("/def", (req, res)=> {
+//     res.send(fs.readFileSync('definition.txt', 'utf8'))
+// });
+
+
+// app.use('/definitions', definitions);
+
+app.listen(5000);
